ac240f331baf41eaa467ccc1f70d6aea4c721fd4
12andrian85 <118369529+12andrian85@users.noreply.github.com>
2023-09-13 19:22:53 +0300

    lyout

--- js/main.js
+++ js/main.js
@@ -4,4 +4,76 @@
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 // Custom JS here
-console.log('hello')
+// console.log('hello')
+var news = new Splide('#news', {
+    type: 'false',
+    perPage: 3,
+    focus: '0',
+    arrows: true,
+    breakpoints: {
+      640: {
+        perPage: 1,
+      },
+      768: {
+        perPage: 1,
+      },
+      1200: {
+        perPage: 1,
+      },
+      2560: {
+        perPage: 1,
+      },
+    }
+  
+  });
+  
+  news.mount();
+
+var projects = new Splide('#projects', {
+    type: 'loop',
+    perPage: 3,
+    focus: '0',
+    arrows: true,
+    breakpoints: {
+      640: {
+        perPage: 1,
+      },
+      768: {
+        perPage: 2,
+      },
+      1200: {
+        perPage: 3,
+      },
+      2560: {
+        perPage: 3,
+      },
+    }
+  
+  });
+  
+  projects.mount();
+
+var support = new Splide('#support', {
+    type: 'loop',
+    autoplay: 'true',
+    perPage: 1,
+    focus: '0',
+    arrows: true,
+    breakpoints: {
+      640: {
+        perPage: 2,
+      },
+      768: {
+        perPage: 3,
+      },
+      1200: {
+        perPage: 5,
+      },
+      2560: {
+        perPage: 5,
+      },
+    }
+  
+  });
+  
+  support.mount();
